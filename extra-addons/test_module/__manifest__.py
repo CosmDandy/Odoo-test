83f842fb94c27f0c80b2a94e206e5c151d43279b
Timofey Kondrashin <tkondrashin@icloud.com>
2025-02-16 15:52:25 +0300

    fix(odoo): Correct typo in security file name

--- extra-addons/test_module/__manifest__.py
+++ extra-addons/test_module/__manifest__.py
@@ -3,7 +3,7 @@
     "version": "1.0",
     "depends": ["base"],
     "author": "CosmDandy",
-    "data": ["views/test_model_views.xml", "security/ir_model_access.csv"],
+    "data": ["views/test_model_views.xml", "security/ir.model.access.csv"],
     "installable": True,
     "application": True,
 }
